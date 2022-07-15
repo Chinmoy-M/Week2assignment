13d9a645e8df479616b6a672a1b89ab75f5fce4c
Chinmoy <chinmoy.malakar99@gmail.com>
2022-07-15 20:07:02 +0530

    Sign up component updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Login from './components/login';
 import Home from './components/home';
+import First from './components/first';
+import Signup from './components/signup';
 
 
 
@@ -14,11 +16,18 @@ function App() {
       <div className="content">
         <Switch>
           <Route exact path = "/">
+            <First/>
+          </Route>
+          <Route path = "/login">
             <Login/>
+          </Route>
+          <Route path = "/signup">
+            <Signup/>
           </Route>
           <Route path = "/home">
             <Home/>
           </Route>
+          <Redirect to = "/" />
         </Switch>
       </div>
     </div>
