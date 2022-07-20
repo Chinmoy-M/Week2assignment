2f27ae636fa655639898a57f669315f3c9864a87
Vibhash Rathi <vibhashrathi@students.vnit.ac.in>
2022-07-20 21:00:26 +0530

    Home page without image...

--- src/components/home.js
+++ src/components/home.js
@@ -3,16 +3,12 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
         <>
-    <div className="container-fluid">
-        <Link to="/"><button className="btn btn-primary">Sign Out </button></Link>
-        </div>
-      
         <div className="mb-4">
   <div className="p-3 bg-info bg-opacity-70 border border-info border-start-0 rounded-end">
-  Welcome to UDB!
+  <strong>Welcome to UDB! </strong> 
   </div>
 
-  <pre>
+  <p>
 
 
   UDB Solutions is a promising startup looking to change the scenario of healthcare
@@ -20,22 +16,24 @@ const Home = () => {
   healthcare facilities, life changingly easy. At UDB, we are always looking to solve
   challenging, real-world problems leveraging today’s new technologies and also
   learning along the way.
-
+ </p>
+ <p>
   As part of our endeavour, we are looking to build a team of young talents with
   technical aptitude and interest in gaining practical knowledge of building an industry
   level application.
-
+  </p>
+  <p>
   We are aware of the lack of opportunities and industry exposure for young
   engineering students. To cater for this issue, we are coming up with a remote
   Internship programme for tech enthusiast undergrad students. Interested students
   can apply for the below roles.
 
-  </pre>
+  </p>
 </div>
   {/* <!-- ...............Grid Starts............ --> */}
 <div className="mb-4">
   <div className="p-3 bg-info bg-opacity-70 border border-info border-start-0 rounded-end">
-    Available Roles
+   <strong> Available Roles </strong>
   </div>
   <br/>
   <br/>
@@ -44,7 +42,7 @@ const Home = () => {
       <div className="col">
         {/* <!-- ........card 1 starts.... --> */}
         <div className="card" >
-          <img src="download (1).png" className="card-img-top" alt="..."/>
+          <img src="download(1).png" className="card-img-top" alt="Image loading error"/>
           <div className="card-body">
             <h5 className="card-title"> UI / UX Designer </h5>
             <p className="card-text">UI/UX Designer would be responsible for creating the wireframes for an application
@@ -70,7 +68,7 @@ const Home = () => {
       <div className="col">
         {/* <!-- ...............Card 3 Starts...................... --> */}
         <div className="card" >
-          <img src="web developper.png" className="card-img-top" alt="..."/>
+          <img src="web developper.png" className="card-img-top" alt="Image loading eror"/>
           <div className="card-body">
             <h5 className="card-title">. Web Developer</h5>
             <p className="card-text">Web Developer is responsible for the following tasks:
@@ -86,6 +84,10 @@ const Home = () => {
 </div>
       
      
+<div className="container-fluid">
+    <Link to="/"><button className="btn btn-primary">Sign Out </button></Link>
+    </div>
+  
       </>
    );
 }
